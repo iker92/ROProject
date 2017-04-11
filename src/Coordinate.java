84dda1715fc9d846b60eb7056382bba81891ee47
iker92 <alex_pili@hotmail.it>
2017-04-11 13:01:05 +0200

    first commit

---
+++ src/Coordinate.java
@@ -0,0 +1,14 @@
+/**
+ * Created by pippo on 11/04/17.
+ */
+public class Coordinate {
+
+    public int x;
+    public int y;
+
+    public Coordinate(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+}
