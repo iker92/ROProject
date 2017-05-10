6caea8425528294d44d0f3bd2da59cb53871f633
iker92 <alex_pili@hotmail.it>
2017-05-10 10:43:19 +0200

    moved writeToFile method to helper class

--- src/Helper.java
+++ src/Helper.java
@@ -1,11 +1,11 @@
 import core.Coordinates;
+import org.apache.commons.io.FileUtils;
 import utils.MaxWeightException;
 import utils.SwapFailedException;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -410,6 +410,41 @@ public class Helper {
         System.out.print("\n");
         System.out.print("\n");
 
+    }
+    public void writeToFile(RouteList routes,long time){
+        File file = new File("/home/pippo/Documents/ROProject/test.txt");
+        ArrayList<String> data=new ArrayList<>();
+        ArrayList<String> routeNodes=new ArrayList<>();
+
+        for (int i = 0; i <routes.size() ; i++) {
+            routeNodes.clear();
+            for (Node n: routes.get(i).nodeList) {
+
+                if(routeNodes.size()!=0){
+
+                    routeNodes.add(String.join(routeNodes.get(0),String.valueOf(n.index+" ")));}
+                else{
+                    routeNodes.add(String.valueOf(n.index+" "));
+                }
+
+
+            }
+            String nodes= (String.join("", routeNodes));
+
+
+            data.add("Route "+i+" cost: "+routes.get(i).getActualDistance()+" \nweigth LINEHAUL :"+routes.get(i).weightLinehaul+" \nweigth BACKHAUL: "+routes.get(i).weightBackhaul+"\nRoute: "+nodes+"\n\n");
+
+        }
+        data.add("Objective function: "+routes.getObjectiveFunction()+"\n");
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        data.add("Execution time is " + formatter.format((time) / 1000000000d) + " seconds");
+
+        try {
+
+            FileUtils.writeLines(file,data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
